3e2a441195fceffd1756a77ed1b0fd5b10aa9b4d
tedrb <70173772+tedrb@users.noreply.github.com>
2020-08-24 13:56:46 -0700

    Update app.js

--- site/app.js
+++ site/app.js
@@ -52,12 +52,12 @@ app.get('/info', function(req, res) {
   if (CURRENTMODE == MODE.TEST) {
     patientdata = {
       "personal": {
-        "name": "Ralph DAlmeida",
-        "age": 38,
+        "name": "George Smiley",
+        "age": 71,
         "gender": "male",
-        "street": "34 Main Street",
-        "city": "Toronto",
-        "zipcode": "M5H 1T1"
+        "street": "6 Bywater St, Chelsea",
+        "city": "London",
+        "zipcode": "SW1"
       },
       "medications": ["Metoprolol", "ACE inhibitors", "Vitamin D"],
       "appointments": ["2018-01-15 1:00 - Dentist", "2018-02-14 4:00 - Internal Medicine", "2018-09-30 8:00 - Pediatry"]
